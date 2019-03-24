e5dd6619243c6d2038d66390d65552458a9645e9
annarudenia <angel-a_013@mail.ru>
2019-03-24 23:51:08 +0300

    new files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-}
+      var base1 = base;
+      var dividers = {};
+      var i = 2;
+      while (base1 > 1) 
+      {
+          if (base1 % i == 0) 
+          {
+              base1 = base1/ i;
+            // don't know how to realise  
+            }
+              
+            else 
+            {
+             i++;
+            }
+      }
+      
+      var allResults = [];
+      for (let divider of Object.keys(dividers)) {
+          var num1 = number;
+          let resultForDivider = 0;
+          while (num1 > 0) {
+              num1 = num1/divider;
+              resultForDivider = resultForDivider+ Math.floor(num1);
+          }
+        allResults.push(Math.floor(resultForDivider / dividers[divider]));
+      }
+     var result = Math.min(...allResults);
+      return result;
+  }
